c73d9a2d7b1f3b10b1a965e771a9ecf4e9084dd0
Vitaliy Vlasov <siphiuel@gmail.com>
2021-03-02 18:53:13 +0200

    Move build[Android, iOS] into separate packages

--- nix/nim-status/default.nix
+++ nix/nim-status/default.nix
@@ -1,8 +1,6 @@
 { mkShell
 , callPackage
 , writeScript
-, writeText
-, xcodeWrapper
 , pkgs
 , stdenv}:
 let
@@ -14,52 +12,9 @@ let
     inherit platform arch;
   };
 
-  buildAndroid = buildMap: name: stdenv.mkDerivation {
-    name = "${name}-android";
-    buildInputs = [ pkgs.coreutils ];
-    builder = writeScript "${name}-android-builder.sh"
-    ''
-      source $stdenv/setup
-      mkdir $out
+  buildAndroid = callPackage ./buildAndroid.nix {};
 
-      ln -s ${buildMap.x86} $out/x86
-      ln -s ${buildMap.arm} $out/armeabi-v7a
-      ln -s ${buildMap.arm64} $out/arm64-v8a
-    '';
-  };
-
-  # Create a single multi-arch fat binary using lipo
-  # Create a single header for multiple target archs
-  # by utilizing C preprocessor conditionals
-  buildIos = buildMap: name:
-    let
-      headerIos = writeText "${name}.h" ''
-        #if TARGET_CPU_X86_64
-        ${builtins.readFile "${buildMap.x86}/${name}.h"}
-        #elif TARGET_CPU_ARM
-        ${builtins.readFile "${buildMap.arm}/${name}.h"}
-        #else
-        ${builtins.readFile "${buildMap.arm64}/${name}.h"}
-        #endif
-      '';
-    in stdenv.mkDerivation {
-      name = "${name}-ios";
-      buildInputs = [ pkgs.coreutils xcodeWrapper ];
-      builder = writeScript "${name}-ios-builder.sh"
-      ''
-        source $stdenv/setup
-        mkdir $out
-
-        # lipo merges arch-specific binaries into one fat iOS binary
-        lipo -create ${buildMap.x86}/lib${name}.a \
-             ${buildMap.arm}/lib${name}.a \
-             ${buildMap.arm64}/lib${name}.a \
-             -output $out/lib${name}.a
-
-        cp ${headerIos} $out/${name}.h
-        ${if name=="nim_status" then "cp ${buildMap.arm64}/nimbase.h $out" else ""}
-      '';
-  };
+  buildIos = callPackage ./buildIos.nix {};
 in rec {
   nim-status = {
     android = {
